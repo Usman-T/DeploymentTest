9273165065d1fe2b5b395201621082158a1eb93d
FSusman <r.usman.tanveer@gmail.com>
2024-08-07 11:50:04 +0500

    the src is now populated

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,15 +1,34 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const resolvers = require("../src/resolvers");
+const typeDefs = require("../src/typeDefs");
+const User = require("../src/models/user");
+const jwt = require("jsonwebtoken");
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
 
 const server = new ApolloServer({
   typeDefs,
-  mocks: true,
-  playground: true // enable GraphQL Playground IDE on prod env
+  resolvers,
+  playground: true,
 });
 
-exports.handler = server.createHandler(); 
+exports.handler = server.createHandler({
+  content: async ( req ) => {
+    const auth = req?.headers?.authorization;
+
+    if (auth?.startsWith("Bearer ")) {
+      const token = auth.substring(7);
+      try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        const currentUser = await User.findById(id);
+
+        return { currentUser };
+      } catch (error) {
+        console.error("JWT Verification Error:", error.message);
+        console.log("Invalid token");
+        return {};
+      }
+    }
+
+    return {};
+  },
+});
