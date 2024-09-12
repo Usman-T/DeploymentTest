90bbd4320d31c79720813c98a293d192a4943297
Usman Tanveer <r.usman.tanveer@gmail.com>
2024-09-12 16:05:05 +0500

    Added the enrolled users

--- src/enrolled.js
+++ src/enrolled.js
@@ -1,3 +1,3 @@
-const enrolled = ["usman.da.dev", "Daniyal Asif", "Ali Tanveer"];
+const enrolled = ["usman.da.dev", "Daniyal Asif", "Aena Khan", "Saheeka Asad", "Nehaj Zehra", "Abdul Hadi", "Barya Kashif", "Mehak Naz", "Farman Ali", "Aiman Shah", "Alishba Fareed", "Ayesha Yousuf", "Aleesha Shabbir", "Nayab Ishfaq", "Wania Kashif", "Anabia Shahwar", "Wanya Khurram", "Azib Mushtaq"];
 
 module.exports = enrolled;
